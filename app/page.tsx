d6d3660e9278986f2ae2df7975fe900047d82064
Piyush Gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2024-06-30 17:38:02 +0530

    chore: Add .prettierignore, .prettierrc, and update package.json

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,29 @@ import EducationSection from "@/components/EducationSection";
 import ContactSection from "@/components/ContactSection";
 
 export default async function Home() {
-    return (
-        <main className="text-black mx-auto max-w-[40rem]">
-            <Header />
-            <main className="flex flex-col px-4">
-                <section className="pb-16">
-                    <h1 className="text-4xl font-semibold mb-4">
-                        Ciao, I&apos;m Piyush.
-                        <br />
-                        <span className="text-4xl font-semibold text-black/60">
-                            A computer science engineer from India.
-                        </span>
-                    </h1>
-                    <p className="text-lg text-black/70">
-                        I am a software developer with a passion for building
-                        web applications and solving complex problems. I have
-                        experience in developing full-stack applications using
-                        modern technologies.
-                    </p>
-                </section>
-                <ProjectsSection />
-                <ExperienceSection />
-                <EducationSection />
-                <ContactSection />
-            </main>
-        </main>
-    );
+  return (
+    <main className="mx-auto max-w-[40rem] text-black">
+      <Header />
+      <main className="flex flex-col px-4">
+        <section className="pb-16">
+          <h1 className="mb-4 text-4xl font-semibold">
+            Ciao, I&apos;m Piyush.
+            <br />
+            <span className="text-4xl font-semibold text-black/60">
+              A computer science engineer from India.
+            </span>
+          </h1>
+          <p className="text-lg text-black/70">
+            I am a software developer with a passion for building web
+            applications and solving complex problems. I have experience in
+            developing full-stack applications using modern technologies.
+          </p>
+        </section>
+        <ProjectsSection />
+        <ExperienceSection />
+        <EducationSection />
+        <ContactSection />
+      </main>
+    </main>
+  );
 }
